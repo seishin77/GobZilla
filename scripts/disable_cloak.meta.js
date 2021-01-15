27a19b435b52c12fa3a6b6093881445367a8ed42
Seishin <ykohler@gmail.com>
2021-01-15 18:27:51 +0100

    Create disable_cloak.meta.js

---
+++ scripts/disable_cloak.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         disable_cloakroom
+// @namespace    http://gobland.fr/
+// @version      0.1
+// @copyright    2020+
+// @description  Disable cloakroom checkbox
+// @author       Seishin
+// @run-at       document-end
+// @match        http://games.gobland.fr/depot.php*
+// @grant        none
+// @downloadURL https://github.com/seishin77/GobZilla/raw/master/scripts/disable_cloak.user.js
+// @updateURL   https://github.com/seishin77/GobZilla/raw/master/scripts/disable_cloak.meta.js
+// ==/UserScript==
